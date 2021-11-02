74abd89fde51bf66d6f4acab8c7bf465b326f045
nghia17800 <lvn17800@gmail.com>
2021-11-02 09:44:17 +0700

    build login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root "static_pages#home"
   get "static_pages/home"
   get "static_pages/help"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
   get "/signup",  to: "users#new"
   resources :users 
 end
