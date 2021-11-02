80e7cd01fc941de103ffd2175213db5935be4d65
nghia17800 <lvn17800@gmail.com>
2021-11-02 10:16:17 +0700

    update, show, delete user

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem "bcrypt", "3.1.12"
+gem "faker"
+gem "will_paginate"
+gem "bootstrap-will_paginate"
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,11 @@ module SessionsHelper
         @current_user = user
       end
     end
-  end   
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
 
   def logged_in?
     current_user.present?
@@ -40,6 +44,15 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
  
