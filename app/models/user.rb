9378053b38de28545fb68d4c34a181aa082c4897
nghia17800 <lvn17800@gmail.com>
2021-11-08 13:15:41 +0700

    update seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: Relationship.name, foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  validates :follower_id, presence: true
-  validates :followed_id, presence: true
   has_secure_password
 
   def feed
