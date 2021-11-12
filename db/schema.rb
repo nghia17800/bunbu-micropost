aa5aa7c53d01fc6cbbdf993d448163ef2e6ae8b2
nghia17800 <lvn17800@gmail.com>
2021-11-12 09:22:47 +0700

    refs #3 Create table, validates, add associations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_075456) do
+ActiveRecord::Schema.define(version: 2021_11_12_014504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.integer "parent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
