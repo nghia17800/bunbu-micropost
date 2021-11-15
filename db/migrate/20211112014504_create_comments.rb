591b226440b74a675028d9f915b8e67f0f75666b
nghia17800 <51740727+nghia17800@users.noreply.github.com>
2021-11-15 08:59:37 +0700

    refs #3 Create table, validates, add associations. (#4)
    * refs #3 Create table, validates, add associations.
    
    * rename parent in comment table

---
+++ db/migrate/20211112014504_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.integer :post_id
+      t.integer :parent_comment_id
+
+      t.timestamps
+    end
+  end
+end
