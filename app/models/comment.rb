63235ef604c3a19e0e5a2788b52d043aad1f89db
nghia17800 <lvn17800@gmail.com>
2021-11-15 09:04:07 +0700

    refs #3 UI Comment in post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-<<<<<<< HEAD
   belongs_to :parent_comment, class_name: 'Comment', optional: true
   has_many :replies, class_name: 'Comment', foreign_key: :parent_comment_id, dependent: :destroy
-=======
-  belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
->>>>>>> refs #3 Create table, validates, add associations.
   validates :content, presence: true
 end
--- db/migrate/20211112014504_create_comments.rb
+++ db/migrate/20211112014504_create_comments.rb
@@ -3,11 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :content
       t.integer :post_id
-<<<<<<< HEAD
       t.integer :parent_comment_id
-=======
-      t.integer :parent_id
->>>>>>> refs #3 Create table, validates, add associations.
 
       t.timestamps
     end
